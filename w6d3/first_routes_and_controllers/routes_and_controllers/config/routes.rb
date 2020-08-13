ea1dcf41fe8acbdd9f046787e0f74224062f9e64
Drew Webster <drewwebs@gmail.com>
2020-08-12 20:47:21 -0400

    Fun day of routing

---
+++ w6d3/first_routes_and_controllers/routes_and_controllers/config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # resources :users
+  
+  get "users/:id", to: "users#show", as: "user"
+  get "users", to: "users#index", as: "users"
+  post "users", to: "users#create"#, as: "user"
+  get "users/new", to: "users#new", as: "new_user"
+  get "users/:id/edit", to: "users#edit", as: "edit_user"
+  patch "users/:id", to: "users#update"#, as: "user"
+  put "users/:id", to: "users#update"#, as: "user"
+  delete "users/:id", to: "users#destroy"#, as: "user"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
