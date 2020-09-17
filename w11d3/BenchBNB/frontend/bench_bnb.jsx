baf3b2672460d19786b6e46b00952faf8ecae138
Drew <drewwebs@gmail.com>
2020-09-17 14:52:34 -0400

    Session actions functional

--- w11d3/BenchBNB/frontend/bench_bnb.jsx
+++ w11d3/BenchBNB/frontend/bench_bnb.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { signup, login, logout } from './util/session_api_util';
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   window.signup = signup;
